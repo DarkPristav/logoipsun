7f72f2dfcf8134ca8ef2e164f38e07bd043ecec9
Petr Atamaniuk <dark.pristav@gmail.com>
2023-09-15 23:38:15 +0200

    main-screen

---
+++ script.js
@@ -0,0 +1,11 @@
+function navToggle() {
+    var navToggleNav = document.getElementById("nav");
+    var html = document.getElementById("html");
+    var nav__btn = document.getElementById("nav__btn");  
+    navToggleNav.classList.toggle("_active");
+    html.classList.toggle("_overflow");
+    nav__btn.classList.toggle("btn");
+}
+
+
+
